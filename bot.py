f56bb37aea8fa7e9d5b33e24a5ed3b7520db1c9d
glowy <83473782+batsoup804@users.noreply.github.com>
2021-11-01 15:32:00 -0700

    bot.py
    okay

---
+++ bot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix = 'itami')
+
+@bot.event
+async def on_ready():
+  await bot.change_presence(activity = discord.Game(name = "with Matches"))
+  print("Bot is ready")
+extentions = ['cogs.event', 'cogs.gen', 'cogs.fun', 'cogs.mod']
+for i in extentions:
+  bot.load_extention(i)
+  pass
+bot.run(token)
