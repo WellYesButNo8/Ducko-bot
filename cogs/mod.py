75b5f200bd65697f4b549e623e5eae23c2d303b9
glowy <83473782+batsoup804@users.noreply.github.com>
2021-11-04 09:40:20 -0700

    switched out checks for decorator and updated file

--- cogs/mod.py
+++ cogs/mod.py
@@ -1,7 +1,6 @@
 #I have too little cogs lol
 import discord
 from discord.ext import commands
-from utils.perms import Checks
 
 class mod(commands.Cog):
   
@@ -9,6 +8,7 @@ class mod(commands.Cog):
     self.bot = bot
    
   @commands.command()
+  @commands.has_permissions(kick_members=True)
   async def kick(self, ctx, member):
     mod = Checks.mod_check(ctx.author)
     if mod == True:
@@ -19,14 +19,11 @@ class mod(commands.Cog):
       
       
    @commands.command()
+   @commands.has_permissions(ban_members=True)
    async def ban(self, ctx, member, reason):
-      mod = Checks.mod_check(ctx.author)
-      if mod==True:
         await Guild.ban(member, reason=reason)
         await ctx.send(f"Successfully banned {member} for {reason}")
-      else:
-        await ctx.send(f"Sorry, you don't have permissions to ban {member}.")
-    
+        
     @commands.command()
     async def ratecheck(self, ctx, member, msgnum = 500):
       #no check here, anyone can use this as it's just a checking command to see how much a user has been active. I added it to moderation as a tool.
@@ -41,18 +38,15 @@ class mod(commands.Cog):
        
   
    @commands.command()
+    @commands.has_permissions(manage_guild=True)
    async def newrole(self, ctx, rolename, color):
-    rolechange = Checks.edit_roles(ctx.author)
-    if rolechange == True:
       await ctx.guild.create_role(name=rolename,colour=discord.Color(color), hoist = True)
-    else: 
-      await ctx.send(f"Sorry, you don't have permissions to create the role {rolename}.")
+      await ctx.send("Role created.")
       
    
    @commands.command()
+   @commands.has_permissions(manage_guild=True)
    async def editrole(self, ctx, role: discord.Role, attr, arg):
-     rolechange = Checks.edit_roles(ctx.author)
-     if rolechange == True:
       if attr == "perms":
         await ctx.send("Sorry, you're unable to change permissions with a command. Kind of useless but sorry ¯\_(ツ)_/¯")
       elif attr == "display":
@@ -69,26 +63,22 @@ class mod(commands.Cog):
         role.edit(color=arg)
       else: 
         await ctx.send("sorry, not an option")
-     else:
-      await ctx.send("Sorry, you don't have permission to change this role.")
         
         
     @commands.command()
+    @commands.has_permissions(manage_guild=True)
     async def hoist(self, ctx, role, num: int):
-      rolechange = Checks.edit_roles(ctx.author)
-      if rolechange == True:
         await guild.edit_role_positions(role.position += num)
         await ctx.send(f"raised role {role}'s position by {num}")
       else:
         await ctx.send("Sorry, you don't have permission") 
      
     @commands.command()
+    @commands.has_permissions(manage_guild=True)
     async def nick(self, ctx, member, name):
       mod = Checks.mod_check(ctx.author)
       if member == ctx.author or mod == True:
           member.edit(nick=name)
-      else:
-        await ctx.send("Sorry, you don't have permissions to nick someone else.")
         
 def setup(bot):
   bot.add_cog(mod(bot))
