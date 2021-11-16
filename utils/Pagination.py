561731e01837dd0626146a543e4cefe5d620c5ec
glowy <83473782+batsoup804@users.noreply.github.com>
2021-11-16 14:45:54 -0800

    update pagination: util classmethods added
    Switch preloaded embeds because it's too slow to build it while it's switching. also adding help commands just adds fields with inline set to false so maybe I'll just make another one that's just "add_field" to add fields to the embed

--- utils/Pagination.py
+++ utils/Pagination.py
@@ -5,14 +5,14 @@ from discord.ext import commands
 
 class Paginator():
   
-  def __init__(self, arg1, arg2 = None, arg3 = None):
+  def __init__(self, ctx, arg1, arg2 = None, arg3 = None):
     self.arg1 = arg1
     self.arg2 = arg2 or None
     self.arg3 = arg3 or None
-    
+    self.ctx = commands.Context
     
   # embed presets
-  @classmethod
+  @classmethod # Error embed in case something didn't turn out.
   def roleError(self, arg1, arg2=None, arg3 = None):
     newemb = discord.Embed(title = arg1, color = 0xD35400)
     if arg2 not None:
@@ -22,14 +22,26 @@ class Paginator():
     return newemb
   
   @classmethod
-  def cmdSuccess(self, arg1):
-    newembed = discord.Embed(title = "Command successfully executed.", description = arg1, color = 0xD3500)
+  def cmdSuccess(self, arg1): # creates a command successful embed with whatever title i specify (could be for a command with a name that's a special case)
+    newembed = discord.Embed(title = arg1, description = "Command successful.", color = 0xD3500)
     return newembed
  
   @classmethod
-  def helpCmd(self, arg1, cat):
+  def helpCmd(self, arg1, cat): # creates a help command embed
     newemb1 = discord.Embed(title = f"Help for {cat} category", description = arg1, color = 0xD3500)
     return newemb1
+  
+  # paginator utils
+  @classmethod #adds help commands (new fields in the embed) in the help embed. Honestly it can be used for anything but it's meant for the help command embed.
+  def add_help_commands(self, arg1, arg2, embed: discord.Embed):
+    embed.add_field(inline = False, name = arg1, value = arg2)
+  
+  @classmethod # switches one embed with another.
+  def switch(self, ctx, embed: discord.Embed, msg: discord.Message):
+    await msg.edit(embed = embed)
+    
+  #that's all my classmethods!
+    
   
 
     
