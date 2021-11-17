a7a96a90509c9ea6694e09a3aeffff5927b65b96
glowy <83473782+batsoup804@users.noreply.github.com>
2021-11-16 19:36:21 -0800

    updated main with hunger games status

--- bot/bot.py
+++ bot/bot.py
@@ -5,6 +5,7 @@ bot = commands.Bot(command_prefix = 'waka')
 
 @bot.event
 async def on_ready():
+  activity = discord.Activity(type = discord.ActivityType.competing, name = "in The Hunger Games!", state = "Hunting down the girl from district 11", party = [13, 24])
   await bot.change_presence(activity = discord.Game(name = "with Matches"))
   print("Bot is ready")
 extentions = ['cogs.guildEvent', 'cogs.fun', 'cogs.mod']
