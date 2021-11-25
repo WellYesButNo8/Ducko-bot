edaf09d219514805c4a8656a7e0c50f7e6742ea0
glowy <83473782+batsoup804@users.noreply.github.com>
2021-11-25 00:19:25 -0800

    added more music commands

--- cogs/music.py
+++ cogs/music.py
@@ -34,5 +34,39 @@ class music(commands.Cog):
       vc.disconnect()
     else:
       pass
+  
+   @commands.command()
+   async def play(self,ctx,url):
+    ctx.voice_client.stop()
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    YDL_OPTIONS = {'format':"bestaudio"}
+    vc = ctx.voice_client
+
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(url, download=False)
+      url2 = info['formats'][0]['url']
+      source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+      vc.play(source)
+    
+   
+  @commands.command()
+  async def pause(self, ctx):
+    await ctx.voice_client.pause()
+    await ctx.message.add_reaction("⏸")
+    await ctx.send("Music paused.")
+    
+  @commands.command()
+  async def resume(self, ctx):
+    await ctx.voice_client.pause()
+    await ctx.message.add_reaction("⏯")
+    await ctx.send("Music resumed.")
+  
+  
+  
+  
+  
+  
+  
+  
       
   
