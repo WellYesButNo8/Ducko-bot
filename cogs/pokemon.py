4367772888c0a0223000da6013d2dddaf17d7abc
Sid <83473782+Heurican@users.noreply.github.com>
2022-04-19 10:15:19 -0700

    started pokemon cog

---
+++ cogs/pokemon.py
@@ -0,0 +1,26 @@
+from discord import ui
+import discord
+from discord.ext import commands
+import requests
+
+class pokebutton(ui.Button):
+    def __init__(self, label=label, row=row, **kwargs):
+        super().__init__(label, row, pokemon, **kwargs)
+
+class Abilities(ui.Button):
+    def callback(self, interaction):
+        view=self.view
+        embed=discord.Embed(title="Abilities", description="all potential abilities this pokemon has")
+        for 
+class Pokemon(commands.Cog):
+    def __init__(self, bot):
+        self.bot=bot
+    
+    
+    
+    @commands.command()
+    async def stat(self, pokemon):
+        poke=requests.get("https://pokeapi.co/api/v2/pokemon/{pokemon}/")
+        sprite=requests.get("")
+        pokembed=discord.Embed(title=poke.name, description="Pokemon number {poke.id}")
+        pokembed.add_field(name="{poke.name}'s potential abilities",description=poke.abilities,inline=False)
