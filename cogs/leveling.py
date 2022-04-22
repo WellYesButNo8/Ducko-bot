972e07095fb8267d422d679d18a1f9a9fabb2695
Sid <83473782+Heurican@users.noreply.github.com>
2022-04-21 17:42:15 -0700

    added leveling

--- cogs/leveling.py
+++ cogs/leveling.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
+import json
+
 
 class Leveling(commands.Cog):
     def __init__(self, bot):
         self.bot=bot
     
     
+
     
 
 def setup(bot):
